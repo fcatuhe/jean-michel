3917648bd8078f5928e1f64ecc4112cd85aba6bd
Francois Catuhe <fcatuhe@gmail.com>
2017-04-06 19:39:49 +0200

    try to fix bug

--- app/bot/views/messages_view.rb
+++ app/bot/views/messages_view.rb
@@ -47,7 +47,7 @@ class Bot::MessagesView
                         elements: [
                           {
                             title: I18n.t('bot.messages.share.title'),
-                            image_url: "https://jean-michel.herokuapp.com#{image_path('messenger_code_400.png')}",
+                            image_url: cl_image_path('messenger_code', width: 400),
                             buttons: [
                               {
                                 type: 'web_url',
